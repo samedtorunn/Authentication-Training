d9dcaafce43adf871e81fd15e4264c8cde990910
Samed Torun <33091148+samedtorunn@users.noreply.github.com>
2022-10-22 14:14:48 +0300

    Level 2 - Encryption of Passwords

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,10 +20,13 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -30,17 +34,23 @@ app.get("/", function(req, res) {
   res.render("home");
 });
 
+
 app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res) {
 
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res) {
 
+app.get("/secrets", function(req, res) {
+  res.render("secrets");
+});
+
+
+app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password
@@ -62,7 +72,7 @@ app.post("/login", function(req, res) {
   User.findOne({email: userName}, function(err, foundOne) {
     if (foundOne) {
       if (foundOne.password === passWord) {
-        res.render("secrets");
+        res.redirect("secrets");
       }  else {
         res.redirect("login");
       }
@@ -72,13 +82,8 @@ app.post("/login", function(req, res) {
   });
 });
 
-app.post("/logout", function(req, res) {
-  res.render("home");
-});
 
 
-
-//TODO
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
