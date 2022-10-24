b72da5a63d5f3fcf191d098feff632f8a04def9f
Samed Torun <33091148+samedtorunn@users.noreply.github.com>
2022-10-24 16:29:27 +0300

    Level 4 - bcrypt package

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 
 const app = express();
@@ -52,30 +54,35 @@ app.get("/secrets", function(req, res) {
 ////// POST METHODS
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password) // we hashed the password while registering.
-  });
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      res.send(err);
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash // we hashed the password while registering.
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        res.send(err);
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res) {
   const userName = req.body.username;
-  const passWord = md5(req.body.password); // we hashed the password input here too, and will compare the registered and login hashes.
+  const passWord = req.body.password; // we hashed the password input here too, and will compare the registered and login hashes.
 
   User.findOne({email: userName}, function(err, foundOne) {
     if (foundOne) {
-      if (foundOne.password === passWord) {
-        res.redirect("secrets");
-      }  else {
-        res.redirect("login");
-      }
+      bcrypt.compare(passWord, foundOne.password, function(err, result) {
+        if(result === true){
+          res.redirect("secrets");
+        } else {
+          res.redirect("login");
+        }
+      });
     } else {
       res.send(err);
     }
